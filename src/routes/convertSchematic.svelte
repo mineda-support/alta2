d4b420d619af5a423b52d4bcfbc73245d023e8dd
seijiro moriyama <seijiro.moriyama@anagix.com>
2025-03-16 21:08:04 +0900

    convertSchematic.svelte WIP: conversion from Edif cont

--- src/routes/convertSchematic.svelte
+++ src/routes/convertSchematic.svelte
@@ -2,6 +2,7 @@
     import { enhance, applyAction } from "$app/forms";
     import { goto } from "$app/navigation";
     import { proj } from "./shared.svelte";
+    import { tooltip, msg } from "./Utils/tooltip.svelte";
     let { port, dir, editor = $bindable() } = $props();
 
     // function convertSchematic(selected) {
@@ -49,6 +50,18 @@
         console.log(`program=${program}`);
         return `dir=${encodeURIComponent(dir)}&program=${program}`;
     }
+    async function convert_edif() {
+        const pickerOpts = {
+            types: [
+                { description: "EDIF(.edif)", accept: { "edif/*": [".edif"] } },
+            ],
+            multiple: false,
+        };
+        let fileHandle;
+        [fileHandle] = await window.showOpenFilePicker(pickerOpts);
+        const file = await fileHandle.getFile();
+        let edifdata = await file.text();
+    }
 </script>
 
 <!-- form method="POST" on:submit={handleSubmit} class='button-2'></form -->
@@ -87,56 +100,21 @@
         {#if editor != " Qucs"}<option value="Qucs">Qucs</option>{/if}
         {#if editor != " Edif"}<option value="Edif">Edif</option>{/if}
     </select>
-</form>
-Current:
-<select bind:value={proj.schema_editor}>
-    <option value="Xschem">Xschem</option>
-    <option value="LTspice">LTspice</option>
-    <option value="EEschema">EEschema</option>
-    <option value="Qucs">Qucs</option>
-    <option value="Edif">Edif</option>
-</select>
-<form
-    method="POST"
-    use:enhance={({ formElement, formData, action, cancel }) => {
-        return async ({ result }) => {
-            proj.schema_editor = selected;
-            console.log("result=", result);
-            // `result` is an `ActionResult` object --- this is not true
-            /* if (result.type === "redirect") {
-                //goto(result.location);
-                window.location = result.location;
-            } else {
-                await applyAction(result);
-            } */
-            if (dir == undefined || dir == "") {
-                alert("Conversion failed --- please read-in the circuit data");
-            } else {
-                alert(`${selected} folder created under ${dir}`);
-            }
-            window.location =
-                "?wdir=" + dir.replace(/^"/, "").replace(/"$/, "") + selected;
-            editor = selected;
-        };
-    }}
-    class="button-2"
-    action={`http://localhost:${port}/api/ltspctl/convert_from_${selected}?${encoded_params(
-        dir,
-        from_program,
-    )}`}
->
-    <button>Convert schematic</button
-    >
-    from
-    <input name="from_program" value={from_program} type="hidden" />
-    <select bind:value={selected}>
-        {#if editor != " Xschem"}<option value="Xschem">Xschem</option>{/if}
-        {#if editor != " LTspice"}<option value="LTspice">LTspice</option>{/if}
-        {#if editor != " EESchema"}<option value="EEschema">EEschema</option
-            >{/if}
-        {#if editor != " Qucs"}<option value="Qucs">Qucs</option>{/if}
-        {#if editor != " Edif"}<option value="Edif">Edif</option>{/if}
+    Current:
+    <select bind:value={proj.schema_editor}>
+        <option value="Xschem">Xschem</option>
+        <option value="LTspice">LTspice</option>
+        <option value="EEschema">EEschema</option>
+        <option value="Qucs">Qucs</option>
+        <option value="Edif">Edif</option>
     </select>
+    {#if proj.schema_editor == 'LTspice'}
+    <button
+        use:tooltip={() => msg("Convert from Edif file")}
+        onclick={() => convert_edif()}
+        class="button-2">Convert from Edif file</button
+    >
+    {/if}
 </form>
 
 <style>
