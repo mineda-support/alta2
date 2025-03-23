5d70f2c08fb8cfc4a3afba54473a59cba6d22de6
Seijiro Moriyama <seijiro.moriyama@anagix.com>
2025-03-24 07:58:07 +0900

    edif_models.svelte for EditModels component added

---
+++ src/routes/Utils/edit_models.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    let { models = $bindable() } = $props();
+    console.log('models in EditModels=', $state.snapshot(models));
+</script>
+
+<div class="tab-content" style="border:green solid 2px;">
+    {#each Object.entries(models) as [model_name, model_params]}
+        [{model_name}]<br />
+        {#each Object.entries(model_params) as [param]}
+            <label
+                >{param}:
+                <input
+                    style="border:darkgray solid 1px;"
+                    bind:value={models[model_name][param]}
+                /><br /></label
+            >
+        {/each}
+    {/each}
+</div>
